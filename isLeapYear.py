35674ae86b50c8163a0c856c877262a7f083c184
huangxu <huangxu@rsdalstar-rd.com>
2016-09-12 15:47:11 +0800

    add isLeapYear first

---
+++ isLeapYear.py
@@ -0,0 +1,16 @@
+﻿##判断是否是闰年
+def isLeapYear(year):
+	if year%400!=0 and year%4!=0:
+		return False
+	else:
+		return True
+		
+def printYear(year):
+	if isLeapYear:
+		print(year,'年是闰年！')
+	else:
+		print(year,'年不是闰年！')
+
+	
+year=2016
+printYear(year)
