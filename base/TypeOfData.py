277c9215b1965e62eac8ef67a4dd903336a528c0
huangxu <huangxu@rsdalstar-rd.com>
2016-09-12 15:57:28 +0800

    update file name

---
+++ base/TypeOfData.py
@@ -0,0 +1,28 @@
+﻿#打印 Hello World
+print('Hello World!')
+
+#tuple和list的主要区别在于,一旦建立,tuple的各个元素不可再变更,而list的各个元素可以再变更
+#s1是tuple定值表，元组
+s1 = (1,2,3,'hello',4,'world',True,5)
+#s2是list表
+s2 = [5,4,3,'hello',2,'world',False,1]
+
+#内置函数type(),查询变量的类型
+print(s1,type(s1))
+print(s2,type(s2))
+
+#集合可以作为另一个集合的元素
+s3 = (1,s1,s2)
+print(s3,type(s3))
+
+#序列的引用通过s[<int>]实现， int为下标
+s2[1]=11
+print(s2[1])
+
+#s1[1]=12
+#print(s1[1])
+
+#范围引用： 基本样式[下限:上限:步长]
+#在范围引用的时候，如果写明上限，那么这个上限本身不包括在内
+s4='abcdefg'
+print(s4[2:6:2])
---
+++ base/hello.py
@@ -0,0 +1,53 @@
+﻿i=1
+
+#if语句
+if i>0:
+	x=1
+	y=2
+elif i==0:
+	x=3
+	y=4
+else:
+	x=5
+	y=6
+print(x,"=====",y)
+
+#for循环
+for i in range(10):
+	j=i+1
+	
+print(j)
+
+#定义函数
+#当没有return, 或者return后面没有返回值时，函数将自动返回None
+#None多用于关键字参数传递的默认值
+def add_1(a,b):
+	c=a+b
+	print(c)
+	print('=====c======')
+	return a,b,c
+	
+print(add_1(3,4))
+print(add_1(3,4)[0])
+
+
+a = 1
+
+def change_integer(a):
+    a = a + 1
+    return a
+	
+#整数变量a不发生变化
+print(change_integer(a))
+print(a)
+
+
+b = [1,2,3]
+
+def change_list(b):
+    b[0] = b[0] + 1
+    return b
+
+print(change_list(b))
+#原来的表b发生变化
+print(b)
